c58ba00e1abcc278ffb498799efa3757b45b9795
xaw <dgasiorowskii@gmail.com>
2023-02-17 10:35:31 +0100

    use generator insted of iteratr

--- src/map_reader/read.py
+++ src/map_reader/read.py
@@ -2,7 +2,7 @@ from typing import TextIO
 
 
 def read_map_file(file_name="input.txt") -> TextIO:
-    try:
-        return open(file_name, "r")
-    except FileNotFoundError:
-        print("File does not exist")
+    with open(file_name, "r") as map_file:
+        for row, line in enumerate(map_file):
+            line = list(line.strip())
+            yield row, line
